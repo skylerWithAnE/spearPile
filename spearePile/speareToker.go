e6308faa386c1737bf3231ed3b7c7c0bb3084b0f
skylerWithAnE <skyler.evans.13@gmail.com>
2017-03-30 16:22:11 -0400

    errors with whitespace in tokenizer

--- spearePile/speareToker.go
+++ spearePile/speareToker.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"regexp"
 	"strings"
 )
 
-var t map[string][]string
+var t map[string]*regexp.Regexp
 var nt map[string][]string
 var currState []string
 var terminalCandidates []*regexp.Regexp
@@ -26,16 +26,13 @@ type token struct {
 	linenum  int
 }
 
-func readTerminals(fname string) (map[string][]string, []*regexp.Regexp) {
+func readTerminals(fname string) map[string]*regexp.Regexp {
 	f, err := os.Open(fname)
 	check(err)
 	defer f.Close()
 
 	r := bufio.NewReader(f)
-
-	var m map[string][]string
-	var re []*regexp.Regexp
-	m = make(map[string][]string)
+	m := make(map[string]*regexp.Regexp)
 
 	for {
 		l, _, err := r.ReadLine()
@@ -46,47 +43,17 @@ func readTerminals(fname string) (map[string][]string, []*regexp.Regexp) {
 		entry := strings.Split(string(l), "->")
 
 		if len(entry) == 2 {
+
 			token := strings.Trim(entry[0], " ")
 			production := strings.Trim(entry[1], " ")
-			m[token] = append(m[token], production)
+
+			fmt.Println(token, production)
+
 			rex, err := regexp.Compile(production)
 			if err == nil {
-				re = append(re, rex)
-			}
-		}
-	}
-
-	return m, re
-}
-
-func readNonTerminals(fname string) map[string][]string {
-	f, err := os.Open(fname)
-	check(err)
-	defer f.Close()
-
-	r := bufio.NewReader(f)
-
-	var m map[string][]string
-	m = make(map[string][]string)
-
-	for {
-		l, _, err := r.ReadLine()
-		if err == io.EOF {
-			break
-		}
-
-		entry := strings.Split(string(l), "->")
-
-		if len(entry) == 2 {
-			token := strings.Trim(entry[0], " ")
-			production := strings.Trim(entry[1], " ")
-			multiProduction := strings.Split(production, "|")
-			if len(multiProduction) > 0 {
-				for i := range multiProduction {
-					m[token] = append(m[token], strings.Trim(multiProduction[i], " "))
-				}
+				m[token] = rex
 			} else {
-				m[token] = append(m[token], production)
+				fmt.Println("Internal error! Production failed to compile to regex!")
 			}
 		}
 	}
@@ -94,137 +61,55 @@ func readNonTerminals(fname string) map[string][]string {
 	return m
 }
 
-func printMap(m map[string][]string) {
+func printMap(m map[string]*regexp.Regexp) {
 	for key, value := range m {
-		for _, production := range value {
-			fmt.Println("Token:\t\t", key, "\nProduction:\t", production)
-		}
+		fmt.Println("Token:\t\t", key, "\nProduction:\t", value, "\n")
 	}
 }
 
-func expandToken(s string) (string, []string) {
-	nontermvalue, nonterminal := nt[s]
-	productions, terminal := t[s]
-	var retval string
-	var expandedToken []string
-	if nonterminal {
-		next := strings.Split(nontermvalue[0], " ")[0]
-		retval, expandedToken = expandToken(next)
-	} else if terminal {
-		for i := range productions {
-			retval = productions[i]
-			expandedToken = strings.Split(s, " ")
+func tokenize(s string) (bool, string, token, int) {
+	// fmt.Println(s)
+	var rstr string
+	var newtoken token
+	linebreaks := 0
+	pass := false
+	for key, value := range t {
+		result := value.FindStringIndex(s)
+		if result != nil && result[0] == 0 {
+			lexstr := s[result[0]:result[1]]
+			rstr = s[result[1]:]
+			newtoken.sym = key
+			newtoken.lex = lexstr
+			pass = true
+			linebreaks = strings.Count(s[:result[1]], "\n")
+			break
 		}
 	}
-	return retval, expandedToken
-}
-
-func currentTokenCandidates() {
-	ntval, nonterm := nt[currState[0]]
-	terminalCandidates = terminalCandidates[:0]
-	if nonterm {
-		for i := range ntval {
-			tval, term := t[ntval[i]]
-			if term {
-				regex, err := regexp.Compile(tval[0])
-				check(err)
-				terminalCandidates = append(terminalCandidates, regex)
-				fmt.Println("Regex: ", regex)
-			}
-		}
-
-	}
-}
-
-func consumeTerminal(s string, terminal string) ([]string, error) {
-	var retval []string
-	_, exists := t[terminal]
-	if !exists {
-		return nil, errors.New("Internal error! Called consumeTerminal, provided terminal: \"" + terminal + "\" not found in map t.")
-	}
-	fmt.Println(s, terminal)
-	nontermvalue, nonterminal := nt[s]
-	if nonterminal {
-		for i := range nontermvalue {
-			regex, err := regexp.Compile(terminal)
-			check(err)
-			result := regex.FindStringIndex(nontermvalue[i])
-			if len(result) > 0 {
-				retval = append(retval, strings.Trim(nontermvalue[i][result[1]:], " "))
-			} else {
-				fmt.Println("This thing ", terminal, " doesn't match this thing: ", nontermvalue[i])
-			}
-
-		}
-	} else {
-		return nil, errors.New("Internal error! Called consumeTerminal(" + s + " ," + terminal + ") where s is a terminal!")
-	}
-	return retval, nil
+	return pass, rstr, newtoken, linebreaks
 }
 
 func main() {
-	var regexList []*regexp.Regexp
-	t, regexList = readTerminals("terminals.txt")
-	nt = readNonTerminals("nonterminals.txt")
-	printMap(t)
-	printMap(nt)
+	t = readTerminals("terminals.txt")
+	// printMap(t)
 
-	fmt.Println("Start state: ", nt["S"])
-
-	var tokens []token
-
-	input, err := os.Open("inputs/11-1.txt")
+	// input, err := os.Open("inputs/11-1.txt")
+	data, err := ioutil.ReadFile("inputs/11-1.txt")
 	check(err)
-	defer input.Close()
-	ir := bufio.NewReader(input)
-
-	var i = 0
-	currState = strings.Split(nt["S"][0], " ")
+	input := string(data)
+	// defer input.Close()
+	// ir := bufio.NewReader(input)
+	linenum := 1
+	var tokens []token
 	for {
-		line, _, readErr := ir.ReadLine()
-		if readErr == io.EOF {
-			break
+		pass, leftover, newtoken, linebreaks := tokenize(input)
+		if pass == false {
+			fmt.Println("Error, could not tokenize input! At line", linenum) //, "\ninput:", input)
+			return
 		}
-		var lstr = string(line)
-
-		// for {
-		// 	if len(currState) > 0 {
-		// 		regexstr, terminalKey := expandToken(currState[0])
-		// 		regex, err := regexp.Compile(regexstr)
-		// 		currentTokenCandidates()
-		// 		check(err)
-		// 		result := regex.FindStringIndex(lstr)
-		// 		if result != nil {
-		// 			fmt.Println("Start index: ", result[0], "\tEnd index: ", result[1])
-		// 			fmt.Println(lstr)
-		// 			lexstr := lstr[result[0]:result[1]]
-		// 			lstr = lstr[result[1]:]
-		// 			fmt.Println(lstr)
-		// 			newState, err := consumeTerminal(currState[0], terminalKey[0])
-		// 			check(err)
-		// 			if len(newState) == 1 {
-		// 				if newState[0] == "" {
-		// 					currState = currState[1:len(currState)]
-		// 				} else {
-		// 					currState[0] = newState[0]
-		// 				}
-
-		// 			} else {
-		// 				fmt.Println("Multiple outcomes...")
-		// 			}
-		// 			fmt.Println(currState)
-
-		// 			var newtoken token
-		// 			newtoken.sym = terminalKey[0]
-		// 			newtoken.lex = lexstr
-		// 			newtoken.linenum = i
-		// 			tokens = append(tokens, newtoken)
-		// 		}
-
-		// 	}
-		// 	if len(lstr) <= 0 {
-		// 		break
-		// 	}
-		// }
+		newtoken.linenum = linenum
+		fmt.Println("new token: SYM:", newtoken.sym, "LEX:", newtoken.lex, "LINE:", newtoken.linenum)
+		tokens = append(tokens, newtoken)
+		input = leftover
+		linenum += linebreaks
 	}
 }
